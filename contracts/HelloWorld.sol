d8371f04e7973f178f5c1bbaaefc1b558f5ece9b
ilovelili <route666@live.cn>
2020-12-04 19:06:11 +0900

    init

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.5;
+
+contract HelloWorld {
+    function greet() public pure returns (string memory) {
+        return "hello world";
+    }
+}
